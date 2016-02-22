102d5e878cbc85fefef5d27bcd68a447b545a9d5
Jodie Alaine Parker <me@jodiealaine.com>
2016-02-22 13:11:55 +0000

    Add ability to colorize Strings (red, green, yellow, blue. pink, light_blue, white, light_grey and black)

---
+++ example.rb
@@ -0,0 +1,22 @@
+require 'colorizr'
+
+# Should colorize the specified text with the color specified
+puts "John".red
+puts "Paul".green
+puts "George".blue
+puts "Ringo".yellow
+puts "John".pink
+puts "Paul".light_blue
+puts "George".white
+puts "Ringo".light_grey
+puts "John".black
+
+# Should only colorize the text with a colorize method called
+puts "Only #{'John'.red} is red"
+
+# Should show a list of color choices avaiable as methods
+# String.colors
+
+# Should show a list text examples for each of our color choice methods
+# String.sample_colors
+
---
+++ lib/colorizr.rb
@@ -0,0 +1,29 @@
+class String
+	def self.create_colors
+		#code to generate all color methods goes here
+		[:red, :green, :yellow, :blue, :pink, :light_blue, :white, :light_grey, :black].each do |c|
+			self.send(:define_method, "#{c}") do
+				"#{colorize_formating c}#{self}#{resume_normal_formating}"
+			end
+		end
+	end
+	def colorize_formating color
+		format = "\e[31m" if color == :red
+		format = "\e[32m" if color == :green
+		format = "\e[33m" if color == :yellow
+		format = "\e[34m" if color == :blue
+		format = "\e[35m" if color == :pink
+		format = "\e[94m" if color == :light_blue
+		format = "\e[97m" if color == :white
+		format = "\e[37m" if color == :light_grey
+		format = "\e[30m" if color == :black
+		format
+	end
+	def resume_normal_formating
+		"\e[0m"
+	end
+end
+
+# Call the create_colors methods so that the colorizr methods 
+# are available when the customizr gem is in installed.
+String.create_colors
